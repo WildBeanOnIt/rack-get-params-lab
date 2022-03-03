783efa661324714aacc9d6574c64ad5f35e63738
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-02 18:14:46 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,54 +1,54 @@
-class Application
+# class Application
 
-  @@items = ["apples","carrots","pears","Carrots"]
-  @@cart = []
+#   @@items = ["apples","carrots","pears","Carrots", "nike dunks"]
+#   @@cart = []
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    #items seach bar
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+#     #items seach bar
+#     if req.path.match(/items/)
+#       @@items.each do |item|
+#         resp.write "#{item}\n"
+#       end
 
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-      #cart search bar
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else 
-        @@cart.each do |cart|
-          resp.write "#{cart}\n"
-        end
-      end
+#     elsif req.path.match(/search/)
+#       search_term = req.params["q"]
+#       resp.write handle_search(search_term)
+#       #cart search bar
+#     elsif req.path.match(/cart/)
+#       if @@cart.empty?
+#         resp.write "Your cart is empty"
+#       else 
+#         @@cart.each do |cart|
+#           resp.write "#{cart}\n"
+#         end
+#       end
 
-    elsif req.path.match(/add/)
-      item = req.params["item"]
-      if @@items.include?(item)
-        @@cart << item
-        resp.write "added #{item}"
-      else
-        resp.write "We don't have that item"
-      end
+#     elsif req.path.match(/add/)
+#       item = req.params["item"]
+#       if @@items.include?(item)
+#         @@cart << item
+#         resp.write "added #{item}"
+#       else
+#         resp.write "We don't have that item"
+#       end
 
-    else
-      resp.write "Path Not Found"
-    end
+#     else
+#       resp.write "Path Not Found"
+#     end
 
-    resp.finish
-  end
+#     resp.finish
+#   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-end
+#   def handle_search(search_term)
+#     if @@items.include?(search_term)
+#       return "#{search_term} is one of our items"
+#     else
+#       return "Couldn't find #{search_term}"
+#     end
+#   end
+# end
 
 # class Application
 
@@ -80,3 +80,20 @@ end
 #       resp.finish
 #     end
 #   end
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
